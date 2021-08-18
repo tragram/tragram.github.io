c2485999ce219e9b2eaab1865fde9e23589dabdf
Dominik Hodan <domho108@gmail.com>
2021-08-18 13:04:04 +0200

    add script to generate a post

---
+++ generate_post.py
@@ -0,0 +1,37 @@
+import datetime
+import sys
+import unicodedata
+import os
+from pytz import country_timezones, timezone
+
+
+def remove_accents(input_str):
+    nfkd_form = unicodedata.normalize('NFKD', input_str)
+    return u"".join([c for c in nfkd_form if not unicodedata.combining(c)])
+
+
+try:
+    name = sys.argv[1]
+except:
+    name = "Příliš žluťoučký kůň úpěl ďábelské ó"
+key_name = remove_accents(name).lower().replace(' ', '_')
+today = datetime.datetime.now(timezone("Europe/Helsinki"))
+filename = f"{datetime.date.today()}-{key_name.replace('_', '-')}"
+post_nr = len(os.listdir("_posts"))
+content = f"""---
+layout: post
+title: {name}
+date: {datetime.date.today()} {today.strftime('%H:%M:%S')} {today.strftime('%z')}
+img_folder: {post_nr:02d}-{key_name}
+description: 
+img: # Add image post (optional)
+fig-caption: # Add figcaption (optional)
+tags: []
+---
+"""
+
+with open(f"_posts/{filename}.markdown", "x", encoding="utf-8") as f:
+    f.write(content)
+img_dir=f"assets/img/{post_nr:02d}-{key_name}"
+if not os.path.exists(img_dir):
+    os.mkdir(img_dir)
